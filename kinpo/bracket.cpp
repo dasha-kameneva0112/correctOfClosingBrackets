4f351774581b0550d29776017b51a554c4068e05
dasha-kameneva0112 <dasha-kameneva969@yandex.ru>
2025-05-03 23:48:39 +0300

    second_version

--- kinpo/bracket.cpp
+++ kinpo/bracket.cpp
@@ -5,5 +5,44 @@ bracket::bracket() {}
 
 bracket::bracket (const QStringList& code, int indexOfLineWithBracket, int position)
 {
+    // Получаем строку и символ скобки
+    const QString line = code[indexOfLineWithBracket];
+    QChar currentBracket = line[position];
+
+    // тип скобки
+    switch (currentBracket.toLatin1())
+    {
+    case ('('):
+    case (')'):
+        type = Round;
+        break;
+    case ('['):
+    case (']'):
+        type = Square;
+        break;
+    case ('{'):
+    case ('}'):
+        type = Curly;
+        break;
+    }
+
+    // сторона скобки
+    switch (currentBracket.toLatin1())
+    {
+    case ('('):
+    case ('['):
+    case ('{'):
+        side = Opening;
+        break;
+    case (')'):
+    case (']'):
+    case ('}'):
+        side = Closing;
+        break;
+    }
+
+    numberOfLine = indexOfLineWithBracket;
+    positionOfBraket = position;
+    correctOfOrder = true;  // По умолчанию считаем порядок правильным
 
 }

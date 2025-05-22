dd2be09d49d070f0681b68d8f9223a49063d663f
dasha-kameneva0112 <dasha-kameneva969@yandex.ru>
2025-05-23 02:23:04 +0300

    fixed_tests

--- testupdateconteinerofbrackets.h
+++ testupdateconteinerofbrackets.h
@@ -12,9 +12,9 @@ private slots:
     void addOpeningBracket(); // №1. Добавление открывающей скобки
     void addCorrectCLosingBracket(); // №2. Добавление закрывающей скобки (без ошибок)
     void addExcessiveClosingBracket(); // №3. Добавление закрывающей скобки (ошибка - нет пары, лишняя закрывающая)
-    void addSeveralExcessiveBrackets(); // №4. В стеке есть несколько (3-4) скобок того же типа
-    void incorrectOrderWithOneBracket(); // №5.1. Неправильный порядок с предыдущей
-    void incorrectOrderWithAllBrackets(); // №5.2. Неправильный порядок с 3-4 всеми предыдущими
+    void addClosingBracketInEmptyContainer(); // №4. Добавление закрывающей скобки в пустой контейнер (ошибка - нет пары, лишняя закрывающая)
+    void incorrectOrderWithOneBracket(); // №5.1. Неправильный порядок с предыдущей (одной)
+    void incorrectOrderWithAllBrackets(); // №5.2. Неправильный порядок со всеми (3-4)  предыдущими
     void incorrectOrderWithSeveralBrackets(); // №5.3. Неправильный порядок с 3-4 предыдущими (не всеми)
     void addMistakeIncorrectOrder(); // №5.4. Добавление ошибки неправильного порядка (порядок уже изменен)
     void addMistakeInNotEmptyConteiner(); // №6. Добавление ошибки в непустой контейнер

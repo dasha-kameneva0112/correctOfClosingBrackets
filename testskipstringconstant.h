22bd5057b38deb09437f6266af4d60c0ffb9e7f4
dasha-kameneva0112 <dasha-kameneva969@yandex.ru>
2025-05-27 00:39:30 +0300

    second_version

--- testskipstringconstant.h
+++ testskipstringconstant.h
@@ -10,15 +10,8 @@ public:
     explicit testskipstringconstant(QObject *parent = nullptr);
 
 private slots:
-    void simpleClosingStringConst(); //№1. простая строковая закрывающаяся константа
-    void escapedQuotationMarkInsideStringConst(); //№2. экранированная кавычка
-    void escapedSlashInsideStringConst(); //№3. экранированный слэш
-    void unclosedStringConst(); //№4. незакрытая строковая константа
-    void IncorrectEscapedQuotationMarkInsideStringConst(); //№5. экранированная кавычка (ошибка)
-    void multilineStringConst(); //№6. перенос строковой константы на другую строку
-    void emptyStringConst(); //№7. пустая строковая константа
-    void incorrectMultilineStringConst(); //№8. перенос строковой константы без слэша (ошибка)
-
+    void add_data();
+    void add();
 };
 
 

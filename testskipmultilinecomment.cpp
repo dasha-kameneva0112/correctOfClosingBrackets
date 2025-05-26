b1377d861b2a38c11f351301d954b279806178c9
dasha-kameneva0112 <dasha-kameneva969@yandex.ru>
2025-05-27 00:41:11 +0300

    second_version

--- testskipmultilinecomment.cpp
+++ testskipmultilinecomment.cpp
@@ -3,145 +3,113 @@
 
 testskipmultilinecomment::testskipmultilinecomment(QObject *parent) : QObject(parent) {}
 
-void testskipmultilinecomment::correctMultilineComment() //№1. корректный многострочный комментарий (на 2 строки)
+
+void testskipmultilinecomment::add_data()
 {
-    QStringList code = {
+    // колонки параметры + ожидемый результат
+    QTest::addColumn<QStringList>("code");
+    QTest::addColumn<int>("currentLine");
+    QTest::addColumn<int>("currentPosition");
+    QTest::addColumn<bool>("resultofskipping");
+    QTest::addColumn<int>("exp_currentLine");
+    QTest::addColumn<int>("exp_currentPosition");
+
+    //Создаем строки‐тесты и заполняем таблицу данными
+    //№1. корректный многострочный комментарий (на 2 строки)
+    QStringList test_code ={
         "int main()",
         "{",
         "int x = 5;",
         "/* comment1",
         "comment2 */",
-        "}"
-    };
-    int currentLine=3;
-    int currentPosition=1;
-    bool resultOfSkipping=skipMultilineComment(code, currentLine, currentPosition);
+        "}" };
+    QTest::newRow("correctMultilineComment") << test_code << 3 << 1 << true << 4 << 10;
 
-    QVERIFY2(resultOfSkipping==true, "Error in resultOfSkipping");
-    QVERIFY2(currentLine==5, "Error in currentLine");
-    QVERIFY2(currentPosition==0, "Error in currentPosition");
-}
-
-void testskipmultilinecomment::correctMultilineCommentInPartOfLine() //№2. корректный многострочный комментарий (часть строки)
-{
-    QStringList code = {
+    //№2. корректный многострочный комментарий (часть строки)
+    test_code = {
         "int main()",
         "{",
         "int /* comment */ x = 5;",
         "}"
     };
-    int currentLine=2;
-    int currentPosition=5;
-    bool resultOfSkipping=skipMultilineComment(code, currentLine, currentPosition);
+    QTest::newRow("correctMultilineCommentInPartOfLine") << test_code << 2 << 5 << true << 2 << 16;
 
-    QVERIFY2(resultOfSkipping==true, "Error in resultOfSkipping");
-    QVERIFY2(currentLine==2, "Error in currentLine");
-    QVERIFY2(currentPosition==16, "Error in currentPosition");
-}
-
-void testskipmultilinecomment::correctMultilineCommentInOneLine() //№3. корректный многострочный комментарий (на одну строку)
-{
-    QStringList code = {
+    //№3. корректный многострочный комментарий (на одну строку)
+    test_code ={
         "int main()",
         "{",
         "/* comment */",
         "int x = 5;",
         "}"
     };
-    int currentLine=2;
-    int currentPosition=1;
-    bool resultOfSkipping=skipMultilineComment(code, currentLine, currentPosition);
+    QTest::newRow("correctMultilineCommentInOneLine") << test_code << 2 << 1 << true << 2 << 12;
 
-    QVERIFY2(resultOfSkipping==true, "Error in resultOfSkipping");
-    QVERIFY2(currentLine==3, "Error in currentLine");
-    QVERIFY2(currentPosition==0, "Error in currentPosition");
-}
-
-void testskipmultilinecomment::unclosedMultilineComment() //№4. незакрытый комментарий
-{
-    QStringList code = {
+    //№4. незакрытый комментарий
+    test_code = {
         "int main()",
         "{",
         "int x = 5;",
         "/* comment ",
         "}"
     };
-    int currentLine=3;
-    int currentPosition=1;
-    bool resultOfSkipping=skipMultilineComment(code, currentLine, currentPosition);
+    QTest::newRow("unclosedMultilineComment") << test_code << 3 << 1 << false << 3 << 1;
 
-    QVERIFY2(resultOfSkipping==false, "Error in resultOfSkipping");
-    QVERIFY2(currentLine==3, "Error in currentLine");
-    QVERIFY2(currentPosition==1, "Error in currentPosition");
-}
-
-void testskipmultilinecomment::emptyMultilineComment() //№5. пустой комментарий
-{
-    QStringList code = {
+    //№5. пустой комментарий
+    test_code = {
         "int main()",
         "{",
         "int x = 5;",
         "/**/",
         "}"
     };
-    int currentLine=3;
-    int currentPosition=1;
-    bool resultOfSkipping=skipMultilineComment(code, currentLine, currentPosition);
+    QTest::newRow("emptyMultilineComment") << test_code << 3 << 1 << true << 3 << 3;
 
-    QVERIFY2(resultOfSkipping==true, "Error in resultOfSkipping");
-    QVERIFY2(currentLine==4, "Error in currentLine");
-    QVERIFY2(currentPosition==0, "Error in currentPosition");
-}
 
-void testskipmultilinecomment::escapedSlashAndAsteriskSymbol() //№6. экранированный слэш и звездочка
-{
-    QStringList code = {
+    //№6. экранированный слэш и звездочка
+    test_code = {
         "int main()",
         "{",
         "int x = 5;",
         "/* *\\/ comment1 */",
         "}"
     };
-    int currentLine=3;
-    int currentPosition=1;
-    bool resultOfSkipping=skipMultilineComment(code, currentLine, currentPosition);
+    QTest::newRow("escapedSlashAndAsteriskSymbol") << test_code << 3 << 1 << true << 3 << 17;
 
-    QVERIFY2(resultOfSkipping==true, "Error in resultOfSkipping");
-    QVERIFY2(currentLine==4, "Error in currentLine");
-    QVERIFY2(currentPosition==0, "Error in currentPosition");
-}
-
-void testskipmultilinecomment::complexTest() //№7. комплексный тест
-{
-    QStringList code = {
+    //№7. комплексный тест
+    test_code = {
         "int main()",
         "{",
         "int /* c*\\///om***me\\\\n*\\/t **/ x = 5;",
         "}"
     };
-    int currentLine=2;
-    int currentPosition=5;
-    bool resultOfSkipping=skipMultilineComment(code, currentLine, currentPosition);
+    QTest::newRow("complexTest") << test_code << 2 << 5 << true << 2 << 30;
 
-    QVERIFY2(resultOfSkipping==true, "Error in resultOfSkipping");
-    QVERIFY2(currentLine==2, "Error in currentLine");
-    QVERIFY2(currentPosition==30, "Error in currentPosition");
-}
-
-void testskipmultilinecomment::multilineCommentInEndOfCode() //№8. комментарий в конце кода (файла)
-{
-    QStringList code = {
+    //№8. комментарий в конце кода (файла)
+    test_code = {
         "int main()",
         "{",
         "int x = 5;",
         "}",
         "/* comment */"
     };
-    int currentLine=5;
-    int currentPosition=1;
-    bool resultOfSkipping=skipMultilineComment(code, currentLine, currentPosition);
-
-    QVERIFY2(resultOfSkipping==true, "Error in resultOfSkipping");
-    QVERIFY2(currentLine==5, "Error in currentLine");
-    QVERIFY2(currentPosition==12, "Error in currentPosition");
+    QTest::newRow("emptyStringConst") << test_code << 4 << 1 << true << 4 << 12;
 }
+
+void testskipmultilinecomment::add()
+{
+    // объект тестируемого класса,чтобы было откуда вызывать методы
+    // изымает из таблицы данные в указанные переменные
+    QFETCH(QStringList, code);
+    QFETCH(int, currentLine);
+    QFETCH(int, currentPosition);
+    QFETCH(bool, resultofskipping);
+    QFETCH(int, exp_currentLine);
+    QFETCH(int, exp_currentPosition);
+
+    // Вызываем метод класса и сравниваем полученное значение с ожидаемым
+    bool result = skipMultilineComment(code, currentLine, currentPosition);
+    QCOMPARE(result, resultofskipping);
+    QCOMPARE(currentLine, exp_currentLine);
+    QCOMPARE(currentPosition, exp_currentPosition);
+}
+

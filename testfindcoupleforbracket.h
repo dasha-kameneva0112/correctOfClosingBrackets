d3e549b23ab19e44d8edb027988d877442c89dea
dasha-kameneva0112 <dasha-kameneva969@yandex.ru>
2025-05-27 00:42:46 +0300

    redesigned_the_tests_usin_data-driven

--- testfindcoupleforbracket.h
+++ testfindcoupleforbracket.h
@@ -8,15 +8,9 @@ class testfindcoupleforbracket : public QObject
     Q_OBJECT //Обязательный макрос
 public:
     explicit testfindcoupleforbracket(QObject *parent = nullptr);
-
-
 private slots:
-    void roundBracket(); // №1.1. Круглая скобка
-    void squareBracket(); // №1.2. Квадратная скобка
-    void curlyBracket(); // №1.3. Фигурная скобка
-    void multipleBracketsOfTheSameType(); // №2. В стеке есть несколько (3-4) скобок того же типа
-    void thereIsNoCouple(); // №3. В стеке нет пары
-    void theStackIsEmpty(); // №4. Стек пустой
+    void add_data(); //В этом слоте создается таблица данных
+    void add(); //  слот за изъятие данных из таблицы и передачу их в тестируемый метод
 };
 
 #endif // TESTFINDCOUPLEFORBRACKET_H

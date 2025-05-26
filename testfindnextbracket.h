52fbd3ea89b867870a0fec97d4a9ed245cbc826a
dasha-kameneva0112 <dasha-kameneva969@yandex.ru>
2025-05-27 00:41:58 +0300

    second_version

--- testfindnextbracket.h
+++ testfindnextbracket.h
@@ -10,16 +10,8 @@ class testfindnextbracket : public QObject
 public:
     explicit testfindnextbracket(QObject *parent = nullptr);
 private slots:
-    void findBracketInBeginOfCode(); //№1. нахождение скобки с начала кода
-    void skipOnelineCommentInCode(); //№2. пропуск однострочного комментария
-    void skipIncorrectOnelineCommentInCode(); //№3. пропуск многострочного комментария (без ошибки)
-    void skipMultilineCommentInCode(); //№4. пропуск многострочного комментария (с ошибкой)
-    void skipIncorrectMultilineCommentInCode(); //№5. пропуск строковой константы (без ошибки)
-    void skipStringConstantInCode(); //№6. пропуск строковой константы (с ошибкой)
-    void skipIncorrectStringConstantInCode(); //№7. пропуск символьной коснтанты (без ошибки)
-    void skipCharConstantInCode(); //№8. пропуск символьной константы (с ошибкой)
-    void skipIncorrectCharConstantInCode(); //№9. комплексный тест - комбинация комментариев и констант
-    void bracketIsNotFound(); //№10. скобка не найдена
+    void add_data(); //В этом слоте создается таблица данных
+    void add(); //  слот за изъятие данных из таблицы и передачу их в тестируемый метод
 };
 
 #endif // TESTFINDNEXTBRACKET_H

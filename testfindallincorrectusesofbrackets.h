be15e153ae21a6ed06c93ca71ecbd83927f953c3
dasha-kameneva0112 <dasha-kameneva969@yandex.ru>
2025-06-01 19:32:32 +0300

    redesigned_the_tests_usin_data-driven

--- testfindallincorrectusesofbrackets.h
+++ testfindallincorrectusesofbrackets.h
@@ -9,17 +9,8 @@ class testfindallincorrectusesofbrackets : public QObject
 public:
     explicit testfindallincorrectusesofbrackets(QObject *parent = nullptr);
 private slots:
-    void findBracketInCorrectCode(); //№1. нахождение скобок в обычном корректном коде
-    void codeWithSkippingConstantsAndCommentsWithBrackets(); //№2. код с комментариями и константами, содержащие скобки (комплексный тест1)
-    void mistakeIsExcessiveClosingBracket(); //№3. ошибка - лишняя закрывающая скобка
-    void mistakeIsUnclosedBracket(); //№4. ошибка - незакрытая скобка
-    void mistakeIsIncorrectOrderOfBrackets(); //№5. ошибка - неправильный порядок
-    void allMistakes(); //№6. комплексный тест2 - комбинация всех ошибок
-    void severalSameMistakes(); //№7. комплексный тест3 - несколько одинаковых ошибок
-    void errorIsUnclosedStringConst(); //№8. ошибка - незакрытая строковая константа
-    void errorIsUnclosedCharConst(); //№9. ошибка - незакрытая символьная константа
-    void errorIsUnclosedMultilineComment(); //№10. ошибка - незакрытый многострочный комментарий
-
+    void add_data();
+    void add();
 };
 
 #endif // TESTFINDALLINCORRECTUSESOFBRACKETS_H

cfa92d0625c2ddce7b7632a91e64bf1f386e6d7b
dasha-kameneva0112 <dasha-kameneva969@yandex.ru>
2025-06-01 19:29:51 +0300

    second_version

--- main.h
+++ main.h
@@ -5,6 +5,16 @@
 #include "bracket.h"
 #include "mistake.h"
 #include "errorininputdata.h"
+#include "testfindcoupleforbracket.h"
+#include "testupdateconteinerofbrackets.h"
+#include "testskipstringconstant.h"
+#include "testskipcharconstant.h"
+#include "testfindallincorrectusesofbrackets.h"
+#include "testfindnextbracket.h"
+#include "testskipmultilinecomment.h"
+#include "testskiponelinecomment.h"
+#include "testconstructorbracket.h"
+
 #include <QSet>
 #include <QStack>
 #include <qDebug>
@@ -12,6 +22,7 @@
 #include <QFileInfo>
 #include <QTextStream>
 #include <QtTest/QtTest>
+#include <QTest>
 
 int readCppFile (const QString& filePath, QStringList& code, errorininputdata& error);
 int findAllIncorrectUsesOfBrackets (const QStringList& code, QSet<mistake>& mistakes, errorininputdata& error);
@@ -20,9 +31,10 @@ void skipOneLineComment (const QStringList& code, int& currentLine, int& current
 bool skipMultilineComment (const QStringList& code, int& currentLine, int& currentPosition);
 bool skipCharConstant (const QStringList& code, int& currentLine, int& currentPosition);
 bool skipStringConstant (const QStringList& code, int& currentLine, int& currentPosition);
-int updateContainerOfBrackets (bracket& newBracket, QStack <bracket>& brackets, QSet<mistake>& mistakes);
 int findCoupleForBracket(const bracket& newBracket, const QStack<bracket>& brackets);
+int updateContainerOfBrackets (bracket& newBracket, QStack <bracket>& brackets, QSet<mistake>& mistakes);
 int generateOutputTxtFile (const QString& filePath, const QStringList& code, QSet<mistake>& mistakes, errorininputdata& errors);
+
 
 
 #endif // MAIN_H

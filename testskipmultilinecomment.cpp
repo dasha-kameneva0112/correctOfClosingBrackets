637b3d72448629a2b50bcb190094061be5dcf4c9
dasha-kameneva0112 <dasha-kameneva969@yandex.ru>
2025-06-01 19:34:25 +0300

    fixed_tests

--- testskipmultilinecomment.cpp
+++ testskipmultilinecomment.cpp
@@ -16,83 +16,84 @@ void testskipmultilinecomment::add_data()
 
     //Создаем строки‐тесты и заполняем таблицу данными
     //№1. корректный многострочный комментарий (на 2 строки)
-    QStringList test_code ={
+    QStringList test_code1 ={
         "int main()",
         "{",
         "int x = 5;",
         "/* comment1",
         "comment2 */",
-        "}" };
-    QTest::newRow("correctMultilineComment") << test_code << 3 << 1 << true << 4 << 10;
+        "}"
+    };
+    QTest::newRow("correctMultilineComment") << test_code1 << 3 << 1 << true << 4 << 10;
 
     //№2. корректный многострочный комментарий (часть строки)
-    test_code = {
+    QStringList test_code2 = {
         "int main()",
         "{",
         "int /* comment */ x = 5;",
         "}"
     };
-    QTest::newRow("correctMultilineCommentInPartOfLine") << test_code << 2 << 5 << true << 2 << 16;
+    QTest::newRow("correctMultilineCommentInPartOfLine") << test_code2 << 2 << 5 << true << 2 << 16;
 
     //№3. корректный многострочный комментарий (на одну строку)
-    test_code ={
+    QStringList test_code3 ={
         "int main()",
         "{",
         "/* comment */",
         "int x = 5;",
         "}"
     };
-    QTest::newRow("correctMultilineCommentInOneLine") << test_code << 2 << 1 << true << 2 << 12;
+    QTest::newRow("correctMultilineCommentInOneLine") << test_code3 << 2 << 1 << true << 2 << 12;
 
     //№4. незакрытый комментарий
-    test_code = {
+    QStringList test_code4 = {
         "int main()",
         "{",
         "int x = 5;",
         "/* comment ",
         "}"
     };
-    QTest::newRow("unclosedMultilineComment") << test_code << 3 << 1 << false << 3 << 1;
+    QTest::newRow("unclosedMultilineComment") << test_code4 << 3 << 1 << false << 3 << 1;
 
     //№5. пустой комментарий
-    test_code = {
+    QStringList test_code5 = {
         "int main()",
         "{",
         "int x = 5;",
         "/**/",
         "}"
     };
-    QTest::newRow("emptyMultilineComment") << test_code << 3 << 1 << true << 3 << 3;
+    QTest::newRow("emptyMultilineComment") << test_code5 << 3 << 1 << true << 3 << 3;
 
 
     //№6. экранированный слэш и звездочка
-    test_code = {
+    QStringList test_code6 = {
         "int main()",
         "{",
         "int x = 5;",
         "/* *\\/ comment1 */",
         "}"
     };
-    QTest::newRow("escapedSlashAndAsteriskSymbol") << test_code << 3 << 1 << true << 3 << 17;
+    QTest::newRow("escapedSlashAndAsteriskSymbol") << test_code6 << 3 << 1 << true << 3 << 17;
 
     //№7. комплексный тест
-    test_code = {
+    QStringList test_code7 = {
         "int main()",
         "{",
         "int /* c*\\///om***me\\\\n*\\/t **/ x = 5;",
         "}"
     };
-    QTest::newRow("complexTest") << test_code << 2 << 5 << true << 2 << 30;
+    QTest::newRow("complexTest") << test_code7 << 2 << 5 << true << 2 << 30;
 
     //№8. комментарий в конце кода (файла)
-    test_code = {
+    QStringList test_code8 = {
         "int main()",
         "{",
         "int x = 5;",
         "}",
         "/* comment */"
     };
-    QTest::newRow("emptyStringConst") << test_code << 4 << 1 << true << 4 << 12;
+    QTest::newRow("emptyStringConst") << test_code8 << 4 << 1 << true << 4 << 12;
 }
 
 void testskipmultilinecomment::add()

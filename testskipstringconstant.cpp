6436904f2ab7a4227619b369e84a70e25e972ab0
dasha-kameneva0112 <dasha-kameneva969@yandex.ru>
2025-06-01 19:35:07 +0300

    second_version

--- testskipstringconstant.cpp
+++ testskipstringconstant.cpp
@@ -14,70 +14,78 @@ void testskipstringconstant::add_data()
 
     //Создаем строки‐тесты и заполняем таблицу данными
     //№1. простая строковая закрывающаяся константа
-    QStringList test_code ={
+    QStringList test_code1 ={
         "int main()",
         "{",
         "QString a = \"Hello, world!\";",
-        "}"  };
-    QTest::newRow("simpleClosingStringConst") << test_code << 2 << 12 << true << 2 << 26;
+        "}"
+    };
+    QTest::newRow("simpleClosingStringConst") << test_code1 << 2 << 12 << true << 2 << 26;
 
     //№2. экранированная кавычка
-    test_code ={
+    QStringList test_code2 ={
         "int main()",
         "{",
         "QString a = \"Hello, \\\"world!\";",
-        "}" };
-    QTest::newRow("escapedQuotationMarkInsideStringConst") << test_code << 2 << 12 << true << 2 << 28;
+        "}"
+    };
+    QTest::newRow("escapedQuotationMarkInsideStringConst") << test_code2 << 2 << 12 << true << 2 << 28;
 
     //№3. экранированный слэш
-    test_code ={
+    QStringList test_code3 ={
         "int main()",
         "{",
         "QString a = \"Hello, world!\\\\\";",
-        "}" };
-    QTest::newRow("escapedSlashInsideStringConst") << test_code << 2 << 12 << true << 2 << 28;
+        "}"
+    };
+    QTest::newRow("escapedSlashInsideStringConst") << test_code3 << 2 << 12 << true << 2 << 28;
 
     //№4. незакрытая строковая константа
-    test_code = {
+    QStringList test_code4 = {
         "int main()",
         "{",
         "QString a = \"Hello, world! ;",
-        "}" };
-    QTest::newRow("unclosedStringConst") << test_code << 2 << 12 << false << 2 << 12;
+        "}"
+    };
+    QTest::newRow("unclosedStringConst") << test_code4 << 2 << 12 << false << 2 << 12;
 
     //№5. экранированная кавычка (ошибка)
-    test_code = {
+    QStringList test_code5 = {
         "int main()",
         "{",
         "QString a = \"Hello, world! \\\\\\\";",
-        "}" };
-    QTest::newRow("IncorrectEscapedQuotationMarkInsideStringConst") << test_code << 2 << 12 << false << 2 << 12;
+        "}"
+    };
+    QTest::newRow("IncorrectEscapedQuotationMarkInsideStringConst") << test_code5 << 2 << 12 << false << 2 << 12;
 
     //№6. перенос строковой константы на другую строку
-    test_code ={
+    QStringList test_code6 ={
         "int main()",
         "{",
         "QString a = \"Hello, \\",
         "world!\";",
-        "}" };
-    QTest::newRow("multilineStringConst") << test_code << 2 << 12 << true << 3 << 6;
+        "}"
+    };
+    QTest::newRow("multilineStringConst") << test_code6 << 2 << 12 << true << 3 << 6;
 
     //№7. пустая строковая константа
-    test_code = {
+    QStringList test_code7 = {
         "int main()",
         "{",
         "QString a = \"\";",
-        "}"  };
-    QTest::newRow("emptyStringConst") << test_code << 2 << 12 << true << 2 << 13;
+        "}"
+    };
+    QTest::newRow("emptyStringConst") << test_code7 << 2 << 12 << true << 2 << 13;
 
     //№8. перенос строковой константы без слэша (ошибка)
-    test_code = {
+    QStringList test_code8 = {
         "int main()",
         "{",
         "QString a = \"Hello, ",
         "world!\";",
-        "}" };
-    QTest::newRow("incorrectMultilineStringConst") << test_code << 2 << 12 << false << 2 << 12;
+        "}"
+    };
+    QTest::newRow("incorrectMultilineStringConst") << test_code8 << 2 << 12 << false << 2 << 12;
 }
 
 void testskipstringconstant::add()

988183b4c4885225ccb3161d64f668a8e43a216f
dasha-kameneva0112 <dasha-kameneva969@yandex.ru>
2025-06-21 22:01:36 +0300

    final_version

--- testskipstringconstant.h
+++ testskipstringconstant.h
@@ -1,3 +1,10 @@
+/*!
+ *\file testskipstringconstant.h
+ *\brief  Заголовочный файл для класса testskipstringconstant
+ *\file testskipstringconstant.cpp
+ *\brief Реализация тестов для функции skipStringConstant
+*/
+
 #ifndef TESTSKIPSTRINGCONSTANT_H
 #define TESTSKIPSTRINGCONSTANT_H
 #include <QObject>
@@ -10,7 +17,20 @@ public:
     explicit testskipstringconstant(QObject *parent = nullptr);
 
 private slots:
+    /*!
+     * \brief add_data - заполнение данными тестовых ситуаций
+     * Проверяет корректность обработки строковых констант:
+     * - Пропуск стандартных строк в двойных кавычках
+     * - Обработку экранированных символов
+     * - Выявление незакрытых строк
+     * - Обработку многострочных строковых констант
+     */
     void add_data();
+
+    /*!
+     * \brief add - выполняет запуск тестовых ситуаций с использованием предоставленных данных.
+     * Выполняет тестирование функции пропуска строковых констант в коде skipStringConstant
+     */
     void add();
 };
 

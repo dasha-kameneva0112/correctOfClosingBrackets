174bb9505229a2320bedcb6c9573bd9482283dd6
dasha-kameneva0112 <dasha-kameneva969@yandex.ru>
2025-06-21 21:39:50 +0300

    final_version

--- testfindallincorrectusesofbrackets.h
+++ testfindallincorrectusesofbrackets.h
@@ -1,3 +1,10 @@
+/*!
+ *\file testfindallincorrectusesofbrackets.h
+ *\brief  Заголовочный файл для класса testfindallincorrectusesofbrackets
+ *\file testfindallincorrectusesofbrackets.cpp
+ *\brief Реализация тестов для функции findAllIncorrectUsesOfBrackets
+*/
+
 #ifndef TESTFINDALLINCORRECTUSESOFBRACKETS_H
 #define TESTFINDALLINCORRECTUSESOFBRACKETS_H
 #include <QObject>
@@ -9,7 +16,21 @@ class testfindallincorrectusesofbrackets : public QObject
 public:
     explicit testfindallincorrectusesofbrackets(QObject *parent = nullptr);
 private slots:
+    /*!
+     * \brief add_data - заполнение данными тестовых ситуаций
+     * Метод создает тестовые случаи, включая:
+     *  - Корректный код со скобками
+     *  - Код с комментариями и константами
+     *  - Различные типы ошибок (лишние, незакрытые, неправильный порядок скобок)
+     *  - Ошибки в строковых и символьных константах
+     *  - Незакрытые многострочные комментарии
+     */
     void add_data();
+
+    /*!
+     * \brief add - выполняет запуск тестовых ситуаций с использованием предоставленных данных.
+     * Выполняет тестирование функции поиска использования некорректных скобок findAllIncorrectUsesOfBrackets
+     */
     void add();
 };
 

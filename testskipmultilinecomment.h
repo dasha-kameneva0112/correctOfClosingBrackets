52223f4c45336c38895b0e78c2cfcf2fdc95d7a2
dasha-kameneva0112 <dasha-kameneva969@yandex.ru>
2025-06-21 21:42:55 +0300

    final_version

--- testskipmultilinecomment.h
+++ testskipmultilinecomment.h
@@ -1,3 +1,10 @@
+/*!
+ *\file testskipmultilinecomment.h
+ *\brief  Заголовочный файл для класса testskipmultilinecomment
+ *\file testskipmultilinecomment.cpp
+ *\brief Реализация тестов для функции skipMultilineComment
+*/
+
 #ifndef TESTSKIPMULTILINECOMMENT_H
 #define TESTSKIPMULTILINECOMMENT_H
 #include <QObject>
@@ -10,7 +17,19 @@ public:
     testskipmultilinecomment(QObject *parent = nullptr);
 
 private slots:
+    /*!
+     * \brief add_data - заполнение данными тестовых ситуаций
+     * Проверяет корректность обработки многострочных комментариев:
+     * - Пропуск корректных комментариев (разных форматов)
+     * - Обработку экранированных символов внутри комментариев
+     * - Выявление незакрытых комментариев
+     */
     void add_data(); //В этом слоте создается таблица данных
+
+    /*!
+     * \brief add - выполняет запуск тестовых ситуаций с использованием предоставленных данных.
+     * Выполняет тестирование функции пропуска многострочных комментариев в коде skipMultilineComment
+     */
     void add(); //  слот за изъятие данных из таблицы и передачу их в тестируемый метод
 };
 

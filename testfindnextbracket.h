28ee2920074eb70933e95ce9566409f6d272b9a1
dasha-kameneva0112 <dasha-kameneva969@yandex.ru>
2025-06-21 21:41:34 +0300

    final_version

--- testfindnextbracket.h
+++ testfindnextbracket.h
@@ -1,3 +1,10 @@
+/*!
+ *\file testfindnextbracket.h
+ *\brief  Заголовочный файл для класса testfindnextbracket
+ *\file testfindnextbracket.cpp
+ *\brief Реализация тестов для функции findNextBracket
+*/
+
 #ifndef TESTFINDNEXTBRACKET_H
 #define TESTFINDNEXTBRACKET_H
 #include <QObject>
@@ -10,8 +17,20 @@ class testfindnextbracket : public QObject
 public:
     explicit testfindnextbracket(QObject *parent = nullptr);
 private slots:
-    void add_data(); //В этом слоте создается таблица данных
-    void add(); //  слот за изъятие данных из таблицы и передачу их в тестируемый метод
+    /*!
+     * \brief add_data - заполнение данными тестовых ситуаций
+     * Проверяет корректность поиска следующей скобки в коде с учетом:
+     * - Пропуска комментариев (однострочных и многострочных)
+     * - Пропуска строковых и символьных констант
+     * - Обработки ошибок в константах и комментариях
+     */
+    void add_data();
+
+    /*!
+     * \brief add - выполняет запуск тестовых ситуаций с использованием предоставленных данных.
+     * Выполняет тестирование функции поиска следующей скобки findNextBracket
+     */
+    void add();
 };
 
 #endif // TESTFINDNEXTBRACKET_H

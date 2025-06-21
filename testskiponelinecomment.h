9d956f5bfa54dfc9f829b9e1b9b9157315c8bc59
dasha-kameneva0112 <dasha-kameneva969@yandex.ru>
2025-06-21 22:01:07 +0300

    final_version

--- testskiponelinecomment.h
+++ testskiponelinecomment.h
@@ -1,3 +1,10 @@
+/*!
+ *\file testskiponelinecomment.h
+ *\brief  Заголовочный файл для класса testskiponelinecomment
+ *\file testskiponelinecomment.cpp
+ *\brief Реализация тестов для функции skipOneLineComment
+*/
+
 #ifndef TESTSKIPONELINECOMMENT_H
 #define TESTSKIPONELINECOMMENT_H
 #include <QObject>
@@ -9,7 +16,20 @@ class testskiponelinecomment : public QObject //Наследование от QO
 public:
     testskiponelinecomment(QObject *parent = nullptr);
 private slots:
+    /*!
+     * \brief add_data - заполнение данными тестовых ситуаций
+     * Проверяет корректность обработки однострочных комментариев:
+     * - Пропуск стандартных комментариев
+     * - Обработку комментариев с экранированными символами
+     * - Пропуск пустых комментариев
+     * - Обработку комментариев в конце кода
+     */
     void add_data(); //В этом слоте создается таблица данных
+
+    /*!
+     * \brief add - выполняет запуск тестовых ситуаций с использованием предоставленных данных.
+     * Выполняет тестирование функции пропуска однострочных комментариев skipOneLineComment
+     */
     void add(); //  слот за изъятие данных из таблицы и передачу их в тестируемый метод
 };
 

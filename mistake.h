68462ba3dc7fbd3358263c6d2ae891e5399980e7
dasha-kameneva0112 <dasha-kameneva969@yandex.ru>
2025-06-21 21:37:56 +0300

    final_version

--- mistake.h
+++ mistake.h
@@ -4,30 +4,71 @@
 #include <QHash>
 #include "bracket.h"
 
+/*!
+ * \enum MistakeWithBracketsType
+ * \brief Типы ошибок, связанных со скобками
+ */
 enum MistakeWithBracketsType{
-    UnclosedBracket,
-    ExcessiveClosingBracket,
-    IncorrectOrderOfBrackets
+    UnclosedBracket, ///< Незакрытая скобка
+    ExcessiveClosingBracket, ///< Избыточная закрывающая скобка
+    IncorrectOrderOfBrackets ///< Неправильный порядок скобок
 };
 
+
+/*!
+ * \class mistake
+ * \brief Класс для хранения информации об ошибках в использовании скобок
+ * Содержит тип ошибки и связанную с ней скобку.
+ */
 class mistake
 {
 public:
+    /*!
+     * \brief mistake - конструктор по умолчанию
+     */
     mistake();
+
+    /*!
+     * \brief mistake - конструктор с параметрами (тип ошибки и скобка)
+     * \param foundBracket - найденная скобка с ошибкой
+     * \param currentType - тип ошибки
+     */
     mistake (bracket foundBracket, MistakeWithBracketsType currentType);
+
+    /*!
+     * \brief getType - метод, который возвращает тип ошибки
+     * \return type - тип ошибки
+     */
     MistakeWithBracketsType getType() const;
+
+    /*!
+     * \brief getBracket - метод, который возвращает скобку с ошибкой
+     * \return bracketWithMistake - скобка с ошибкой
+     */
     bracket getBracket() const;
 
+    /*!
+     * \brief operator == - оператор сравнения ошибок со скобками
+     * \param other - сравниваемый объект mistake
+     * \return true, если ошибки идентичны, false - если нет
+     */
     bool operator==(const mistake& other) const
     {
         return (type == other.type && bracketWithMistake == other.bracketWithMistake);
     }
 
 private:
-    MistakeWithBracketsType type;
-    bracket bracketWithMistake;
+    MistakeWithBracketsType type; ///< Тип ошибки
+    bracket bracketWithMistake; ///< Скобка с ошибкой
 };
 
+
+/*!
+ * \brief qHash - хеш-функция для класса mistake, необходимая для использования mistake в QSet
+ * \param key - объект ошибки
+ * \param seed - начальное значение хеша
+ * \return seed - хеш-значение
+ */
 inline size_t qHash(const mistake& key, size_t seed = 0) noexcept
 {
     // Комбинируем хэши всех полей, которые участвуют в operator==

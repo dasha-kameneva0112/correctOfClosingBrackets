71c229b77370e34242bbdae3e2daf5094999bc5f
dasha-kameneva0112 <dasha-kameneva969@yandex.ru>
2025-06-21 22:08:13 +0300

    final_version

--- testupdateconteinerofbrackets.h
+++ testupdateconteinerofbrackets.h
@@ -1,3 +1,10 @@
+/*!
+ *\file testupdateconteinerofbrackets.h
+ *\brief  Заголовочный файл для класса testupdateconteinerofbrackets
+ *\file testupdateconteinerofbrackets.cpp
+ *\brief Реализация тестов для функции updateContainerOfBrackets
+*/
+
 #ifndef TESTUPDATECONTEINEROFBRACKETS_H
 #define TESTUPDATECONTEINEROFBRACKETS_H
 #include <QObject>
@@ -9,7 +16,20 @@ class testupdateconteinerofbrackets : public QObject
 public:
     explicit testupdateconteinerofbrackets(QObject *parent = nullptr);
 private slots:
+    /*!
+     * \brief add_data - заполнение данными тестовых ситуаций
+     * Проверяет корректность обновления стека скобок и обнаружения ошибок:
+     * - Добавление открывающих и закрывающих скобок
+     * - Обнаружение лишних закрывающих скобок
+     * - Проверка правильности порядка скобок
+     * - Обработка комплексных случаев вложенности
+     */
     void add_data();
+
+    /*!
+     * \brief add - выполняет запуск тестовых ситуаций с использованием предоставленных данных.
+     * Выполняет тестирование функции обновления контейнера скобок и обнаружения ошибок updateContainerOfBrackets
+     */
     void add();
 };
 
